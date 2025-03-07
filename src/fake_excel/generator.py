e66d86976139f254028ba99937081e699bcda27b
Manel Caireta <m.caireta@altostratus.es>
2025-03-07 09:34:07 +0100

    chore: allow `ExcelFaker` to create a record generator

--- src/fake_excel/generator.py
+++ src/fake_excel/generator.py
@@ -1,13 +1,51 @@
+from collections.abc import Callable
+from typing import Iterator
+
+from faker import Faker
+
+fake = Faker()
+
+type_to_generator = {
+    "name": fake.name,
+    "email": fake.email,
+    "phone": fake.phone_number,
+    "address": fake.address,
+    "date": fake.date,
+    "time": fake.time,
+    "datetime": fake.date_time,
+    "text": fake.text,
+    "integer": fake.random_int,
+    "float": fake.random_number,
+    "boolean": fake.boolean,
+    "url": fake.url,
+    "ipv4": fake.ipv4,
+    "ipv6": fake.ipv6,
+    "uuid": fake.uuid4,
+}
+
+
 class ExcelFieldFaker:
     def __init__(self, field_name: str, field_type: str) -> None:
-        self._name = field_name
-        self._type = field_type
-        self._record_generator = None
+        self.name = field_name
+        self._type = field_type.lower()
+        self._record_creator = None
+
+    def get_record(self) -> str:
+        if self._record_creator is None:
+            self._record_creator = self._get_record_creator()
+        return str(self._record_creator())
+
+    def _get_record_creator(self) -> Callable[[], str]:
+        return type_to_generator.get(self._type, lambda *_args, **_kwargs: "NULL")
 
 
 class ExcelFaker:
     def __init__(self, schema: list[dict[str, str]]) -> None:
         self._schema = schema
+
+    def create_fake_excel(self) -> Iterator[dict[str, str]]:
+        while True:
+            yield {field.name: field.get_record() for field in self.get_fields()}
 
     def get_fields(self) -> list[ExcelFieldFaker]:
         return [self._parse_field(field) for field in self._schema]
