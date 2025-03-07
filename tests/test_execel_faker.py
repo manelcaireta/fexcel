7f645a6606d7d8d3141d8bb307a73f7ef6bd70e2
Manel Caireta <m.caireta@altostratus.es>
2025-03-07 09:28:59 +0100

    test: add minor test for main `ExcelFaker` functionality

---
+++ tests/test_execel_faker.py
@@ -0,0 +1,22 @@
+import json
+from collections.abc import Iterator
+from pathlib import Path
+
+from fake_excel.generator import ExcelFaker
+
+
+def test_create_fake_excel(schemas_path: Path) -> None:
+    with (schemas_path / "test.json").open("r") as f:
+        json_schema = json.load(f)
+
+    excel_faker = ExcelFaker(json_schema.get("schema"))
+    iterator = excel_faker.create_fake_excel()
+
+    assert isinstance(iterator, Iterator)
+    record = next(iterator)
+
+    assert isinstance(record, dict)
+    assert all(
+        isinstance(key, str) and isinstance(value, str)
+        for key, value in record.items()
+    )
