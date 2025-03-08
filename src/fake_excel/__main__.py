81d91ead50b00bcaef92856d85af89bfb0fd3bac
Manel Caireta <m.caireta@altostratus.es>
2025-03-08 11:05:33 +0100

    chore: parse the arguments with a `BaseModel` subclass

--- src/fake_excel/__main__.py
+++ src/fake_excel/__main__.py
@@ -1,9 +1,18 @@
 import sys
-from argparse import ArgumentParser, Namespace
+from argparse import ArgumentParser
 
+from pydantic import BaseModel, ConfigDict
 from pyexcel import isave_as
 
 from fake_excel.generator import ExcelFaker
+
+
+class Args(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+
+    schema_path: str
+    output_path: str
+    num_fakes: int
 
 
 def main() -> None:
@@ -13,7 +22,7 @@ def main() -> None:
     isave_as(records=iterator, dest_file_name=args.output_path, sheet_name="Sheet 1")
 
 
-def parse_args() -> Namespace:
+def parse_args() -> Args:
     parser = ArgumentParser()
     parser.add_argument("schema_path", type=str, help="Path to the schema file")
     parser.add_argument("output_path", type=str, help="Path to the output file")
@@ -26,7 +35,7 @@ def parse_args() -> Namespace:
     )
 
     try:
-        return parser.parse_args()
+        return Args.model_validate(parser.parse_args())
     except SystemExit:
         print()
         parser.print_help()
