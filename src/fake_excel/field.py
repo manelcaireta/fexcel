60f145e09afd8072cc0b8301411058741a5b2647
Manel Caireta <m.caireta@altostratus.es>
2025-03-11 13:59:09 +0100

    chore: add min and max constraints on numeric types

--- src/fake_excel/field.py
+++ src/fake_excel/field.py
@@ -1,10 +1,37 @@
+import random
+import sys
+import typing
 from collections.abc import Callable
+from functools import partial
 
 from faker import Faker
 
-from fake_excel.constraint import FieldConstraint
+from fake_excel.constraint import FieldConstraint, NumericConstraint
 
 fake = Faker()
+
+
+def random_float(
+    min_value: float | None = None,
+    max_value: float | None = None,
+) -> float:
+    if min_value is None:
+        min_value = -sys.float_info.min
+    if max_value is None:
+        max_value = sys.float_info.max
+    return random.uniform(min_value, max_value)  # noqa: S311
+
+
+def random_int(
+    min_value: int | None = None,
+    max_value: int | None = None,
+) -> int:
+    if min_value is None:
+        min_value = -sys.maxsize - 1
+    if max_value is None:
+        max_value = sys.maxsize
+    return random.randint(min_value, max_value)  # noqa: S311
+
 
 type_to_generator = {
     "name": fake.name,
@@ -15,9 +42,9 @@ type_to_generator = {
     "time": fake.time,
     "datetime": fake.date_time,
     "text": fake.text,
-    "int": fake.random_int,
-    "integer": fake.random_int,
-    "float": lambda: fake.random_number() / (fake.random_number() or 10),
+    "int": random_int,
+    "integer": random_int,
+    "float": random_float,
     "boolean": fake.boolean,
     "url": fake.url,
     "ipv4": fake.ipv4,
@@ -45,11 +72,26 @@ class ExcelFieldFaker:
             self._value_creator = self._get_value_creator()
         return str(self._value_creator())
 
-    def _get_value_creator(self) -> Callable[[], str]:
+    def _get_value_creator(self) -> Callable[[], str | float]:
         if self._constraints.allowed_values is not None:
-            values = self._constraints.allowed_values.copy()
-            return lambda: fake.random_element(values)
+            return self._value_creator_from_allowed_values()
+        if isinstance(self._constraints, NumericConstraint):
+            return self._value_creator_from_range()
         return type_to_generator.get(self._type, lambda *_args, **_kwargs: "NULL")
+
+    def _value_creator_from_allowed_values(self) -> Callable[[], str]:
+        values = typing.cast(list, self._constraints.allowed_values).copy()
+        return lambda: fake.random_element(values)
+
+    def _value_creator_from_range(self) -> Callable[[], str | float]:
+        self._constraints = typing.cast(NumericConstraint, self._constraints)
+
+        func = type_to_generator.get(self._type, random_int)
+        if self._constraints.min_value is not None:
+            func = partial(func, min_value=self._constraints.min_value)
+        if self._constraints.max_value is not None:
+            func = partial(func, max_value=self._constraints.max_value)
+        return func
 
     def __eq__(self, value: object) -> bool:
         if not isinstance(value, ExcelFieldFaker):
