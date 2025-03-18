6b3db0d20ad947e6d2109eebab32cfb10a704e4f
Manel Caireta <m.caireta@altostratus.es>
2025-03-18 12:34:47 +0100

    refactor: further modularize `fieldfaker` tests

--- tests/fields/test_factory.py
+++ tests/fields/test_factory.py
@@ -3,11 +3,11 @@ import re
 import pytest
 from fake_excel.fields import ExcelFieldFaker
 
-from tests.test_excel_field_test_table import TestCase, test_cases
+from tests.fields.test_factory_test_table import TestCase, test_cases
 
 
 @pytest.mark.parametrize("tt", test_cases * 5)
-def test_excel_field_factory(tt: TestCase) -> None:
+def test_field_faker_factory(tt: TestCase) -> None:
     field_faker = ExcelFieldFaker.parse_field(
         tt.input.name,
         tt.input.type,
@@ -18,15 +18,7 @@ def test_excel_field_factory(tt: TestCase) -> None:
     expected = re.compile(tt.output.pattern)
     assert re.match(expected, actual) is not None
 
-def test_excel_field_factory_invalid_type() -> None:
+
+def test_field_faker_factory_invalid_type() -> None:
     with pytest.raises(ValueError, match="Unknown field type: INVALID_TYPE"):
         ExcelFieldFaker.parse_field("Test", "INVALID_TYPE")
-
-
-def test_excel_fields_equality() -> None:
-    faker = ExcelFieldFaker.parse_field("Test", "INTEGER")
-
-    assert faker == ExcelFieldFaker.parse_field("Test", "INTEGER")
-    assert faker is not ExcelFieldFaker.parse_field("Test", "INTEGER")
-    assert faker != ExcelFieldFaker.parse_field("Test", "TEXT")
-    assert faker != "Not a FieldFaker instance"
