ac529a3ac2a3b6d2080702fe5db85a6dbb19542f
Manel Caireta <m.caireta@altostratus.es>
2025-03-22 20:04:56 +0100

    refactor: update network fakers to not use constraints

--- src/fake_excel/fields/network.py
+++ src/fake_excel/fields/network.py
@@ -1,6 +1,5 @@
 from faker import Faker
 
-from fake_excel.constraint import FieldConstraint
 from fake_excel.fields.base import ExcelFieldFaker
 
 fake = Faker()
@@ -10,30 +9,12 @@ class URLFieldFaker(ExcelFieldFaker, faker_types="url"):
     def get_value(self) -> str:
         return fake.url()
 
-    def parse_constraints(
-        self,
-        constraints: dict | None = None,
-    ) -> FieldConstraint | None:
-        return super().parse_constraints(constraints)
-
 
 class IPv4FieldFaker(ExcelFieldFaker, faker_types="ipv4"):
     def get_value(self) -> str:
         return fake.ipv4()
 
-    def parse_constraints(
-        self,
-        constraints: dict | None = None,
-    ) -> FieldConstraint | None:
-        return super().parse_constraints(constraints)
-
 
 class IPv6FieldFaker(ExcelFieldFaker, faker_types="ipv6"):
     def get_value(self) -> str:
         return fake.ipv6()
-
-    def parse_constraints(
-        self,
-        constraints: dict | None = None,
-    ) -> FieldConstraint | None:
-        return super().parse_constraints(constraints)
