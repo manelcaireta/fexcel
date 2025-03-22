44eddc88dab986fa06a0179eeca8522d81c0e02d
Manel Caireta <m.caireta@altostratus.es>
2025-03-22 20:28:06 +0100

    refactor: update temporal fakers to not use constraints

--- src/fake_excel/fields/temporal.py
+++ src/fake_excel/fields/temporal.py
@@ -1,68 +1,69 @@
-from datetime import date, datetime, timezone
+from datetime import datetime, timezone
 
 from faker import Faker
 
-from fake_excel.constraint import FieldConstraint, TemporalConstraint
 from fake_excel.fields.base import ExcelFieldFaker
 
 fake = Faker()
 
 
-class DateFieldFaker(ExcelFieldFaker, faker_types="date"):
-    constraints: TemporalConstraint
+class DateTimeFieldFaker(ExcelFieldFaker, faker_types=["datetime", "timestamp"]):
+    def __init__(
+        self,
+        field_name: str,
+        *,
+        start_date: str | datetime | None = None,
+        end_date: str | datetime | None = None,
+        format_string: str = "%Y-%m-%d %H:%M:%S",
+    ) -> None:
+        self.format_string = format_string
+
+        if isinstance(start_date, str):
+            start_date = self._try_parse_datetime(start_date)
+        self.start_date = start_date
+
+        if isinstance(end_date, str):
+            end_date = self._try_parse_datetime(end_date)
+        self.end_date = end_date
+
+        super().__init__(field_name)
+
+    def _try_parse_datetime(self, value: str) -> datetime | None:
+        try:
+            return datetime.strptime(value, self.format_string).astimezone(timezone.utc)
+        except ValueError:
+            return datetime.fromisoformat(value)
 
     def get_value(self) -> str:
-        return self.random_date().strftime("%Y-%m-%d")
+        return self.random_datetime().strftime(self.format_string)
 
-    def random_date(self) -> date:
-        start_date = self.constraints.start_date or date(1970, 1, 1)
-        end_date = self.constraints.end_date or datetime.now(timezone.utc).date()
-        return fake.date_time_between(start_date, end_date)
+    def random_datetime(self) -> datetime:
+        epoch = datetime(1970, 1, 1, 0, 0, 0, 0, timezone.utc)
+        start_value = self.start_date or epoch
+        end_value = self.end_date or datetime.now(timezone.utc)
+        return fake.date_time_between(start_value, end_value)
 
-    def parse_constraints(
+
+class DateFieldFaker(DateTimeFieldFaker, faker_types="date"):
+    def __init__(
         self,
-        constraints: dict | None = None,
-    ) -> FieldConstraint | None:
-        if not isinstance(constraints, dict):
-            return TemporalConstraint()
-        return TemporalConstraint(
-            start_date=constraints.get("start_date"),
-            end_date=constraints.get("end_date"),
-            format_string=constraints.get("format_string", "%Y-%m-%d"),
+        field_name: str,
+        *,
+        start_date: str | datetime | None = None,
+        end_date: str | datetime | None = None,
+        format_string: str = "%Y-%m-%d",
+    ) -> None:
+        super().__init__(
+            field_name=field_name,
+            start_date=start_date,
+            end_date=end_date,
+            format_string=format_string,
         )
+
+    def get_value(self) -> str:
+        return self.random_datetime().date().strftime(self.format_string)
 
 
 class TimeFieldFaker(ExcelFieldFaker, faker_types="time"):
     def get_value(self) -> str:
         return fake.time()
-
-    def parse_constraints(
-        self,
-        constraints: dict | None = None,
-    ) -> FieldConstraint | None:
-        return super().parse_constraints(constraints)
-
-
-class DateTimeFieldFaker(ExcelFieldFaker, faker_types=["datetime", "timestamp"]):
-    constraints: TemporalConstraint
-
-    def get_value(self) -> str:
-        return self.random_datetime().strftime(self.constraints.format_string)
-
-    def random_datetime(self) -> datetime:
-        epoch = datetime(1970, 1, 1, 0, 0, 0, 0, timezone.utc)
-        start_value = self.constraints.start_date or epoch
-        end_value = self.constraints.end_date or datetime.now(timezone.utc)
-        return fake.date_time_between(start_value, end_value)
-
-    def parse_constraints(
-        self,
-        constraints: dict | None = None,
-    ) -> FieldConstraint | None:
-        if not isinstance(constraints, dict):
-            return TemporalConstraint(format_string="%Y-%m-%d %H:%M:%S")
-        return TemporalConstraint(
-            start_date=constraints.get("start_date"),
-            end_date=constraints.get("end_date"),
-            format_string=constraints.get("format_string", "%Y-%m-%d %H:%M:%S"),
-        )
