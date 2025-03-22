3801c91836e749e319342e48b7d3790bb145a680
Manel Caireta <m.caireta@altostratus.es>
2025-03-22 17:45:28 +0100

    test: add field registry test

---
+++ tests/fields/test_registry.py
@@ -0,0 +1,28 @@
+import pytest
+
+from fake_excel.constraint import FieldConstraint
+from fake_excel.fields import ExcelFieldFaker
+
+
+def test_field_registration() -> None:
+    class MockFieldFaker(ExcelFieldFaker, faker_types="test_field"):
+        def get_value(self) -> str:
+            return "test_value"
+
+        def parse_constraints(self, constraints: dict) -> FieldConstraint | None:
+            return super().parse_constraints(constraints)
+
+    field_faker = ExcelFieldFaker.parse_field(
+        field_name="mock",
+        field_type="test_field",
+    )
+
+    assert isinstance(field_faker, MockFieldFaker)
+    assert field_faker.get_value() == "test_value"
+    assert field_faker.parse_constraints({}) is None
+
+
+def test_invalid_field_registration() -> None:
+    with pytest.raises(TypeError):
+
+        class MockFieldFaker(ExcelFieldFaker): ...  # type: ignore[]
