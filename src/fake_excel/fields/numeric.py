4f073bce49d886c262a16cce9532ac13533e00fb
Manel Caireta <m.caireta@altostratus.es>
2025-03-22 20:17:12 +0100

    refactor: remove constraints in numeric field fakers

--- src/fake_excel/fields/numeric.py
+++ src/fake_excel/fields/numeric.py
@@ -1,48 +1,86 @@
+import random
+from functools import partial
+
 from faker import Faker
 
-from fake_excel.constraint import FieldConstraint, NumericConstraint
 from fake_excel.fields.base import ExcelFieldFaker
 
 fake = Faker()
+INFINITY = 1e30
+"""
+A very large number but not large enough to mess with RNGs.
+
+Using something like `sys.float_info.max` or `math.inf` can make the RNGs respond
+with `math.inf`.
+"""
 
 
-class IntegerFieldFaker(ExcelFieldFaker, faker_types=["int", "integer"]):
-    constraints: NumericConstraint
+class FloatFieldFaker(ExcelFieldFaker, faker_types="float"):
+    INTERVAL_DISTRIBUTIONS = ("uniform",)
+    EXPONENTIAL_DISTRIBUTIONS = ("normal", "gaussian", "lognormal")
+
+    def __init__(  # noqa: PLR0913
+        self,
+        field_name: str,
+        *,
+        min_value: float | None = None,
+        max_value: float | None = None,
+        mean: float | None = None,
+        std: float | None = None,
+        distribution: str | None = None,
+    ) -> None:
+        self.is_min_max = bool(min_value is not None or max_value is not None)
+        self.is_mean_std = bool(mean is not None or std is not None)
+        self.distribution = distribution or "uniform"
+        self.min_value = min_value if min_value is not None else -INFINITY
+        self.max_value = max_value if max_value is not None else INFINITY
+        self.mean = mean if mean is not None else 0
+        self.std = std if std is not None else 1
+
+        self._raise_if_invalid_combination()
+        self._resolve_rng()
+
+        super().__init__(field_name)
 
     def get_value(self) -> str:
-        return str(int(self.constraints.rng()))
+        return str(self.rng())
 
-    def parse_constraints(
-        self,
-        constraints: dict | None = None,
-    ) -> FieldConstraint | None:
-        if not isinstance(constraints, dict):
-            return NumericConstraint()
-        return NumericConstraint(
-            distribution=constraints.get("distribution"),
-            min_value=constraints.get("min_value"),
-            max_value=constraints.get("max_value"),
-            mean=constraints.get("mean"),
-            std=constraints.get("std"),
-        )
+    def _raise_if_invalid_combination(self) -> None:
+        if (self.is_min_max) and (self.is_mean_std):
+            msg = "Cannot specify both min_value/max_value and mean/std"
+            raise ValueError(msg)
+
+        if (self.is_min_max) and (self.distribution in self.EXPONENTIAL_DISTRIBUTIONS):
+            msg = (
+                "Cannot specify min_value/max_value with "
+                f"{self.distribution} distribution"
+            )
+            raise ValueError(msg)
+
+        if (self.is_mean_std) and (self.distribution in self.INTERVAL_DISTRIBUTIONS):
+            msg = f"Cannot specify mean/std with {self.distribution} distribution"
+            raise ValueError(msg)
+
+        if self.min_value > self.max_value:
+            msg = "min_value must be less than or equal than max_value"
+            raise ValueError(msg)
+
+    def _resolve_rng(self) -> None:
+        match self.distribution.lower():
+            case "uniform":
+                self.rng = partial(random.uniform, self.min_value, self.max_value)
+            case "normal":
+                self.rng = partial(random.normalvariate, self.mean, self.std)
+            case "gaussian":
+                self.rng = partial(random.gauss, self.mean, self.std)
+            case "lognormal":
+                self.rng = partial(random.lognormvariate, self.mean, self.std)
+            case _:
+                msg = f"Invalid distribution: {self.distribution}"
+                raise ValueError(msg)
 
 
-class FloatFieldFaker(ExcelFieldFaker, faker_types=["float", "double"]):
-    constraints: NumericConstraint
-
+# NOTE: If Python allows `int` to be treated as a `float` then I will too
+class IntegerFieldFaker(FloatFieldFaker, faker_types=["int", "integer"]):
     def get_value(self) -> str:
-        return str(self.constraints.rng())
-
-    def parse_constraints(
-        self,
-        constraints: dict | None = None,
-    ) -> FieldConstraint | None:
-        if not isinstance(constraints, dict):
-            return NumericConstraint()
-        return NumericConstraint(
-            distribution=constraints.get("distribution"),
-            min_value=constraints.get("min_value"),
-            max_value=constraints.get("max_value"),
-            mean=constraints.get("mean"),
-            std=constraints.get("std"),
-        )
+        return str(int(self.rng()))
